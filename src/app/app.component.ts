be80259822f3cc98eb4975cfbb5fa31d8f4b5ee2
lotar <lotar@localhost.localdomain>
2024-08-01 15:42:06 -0300

    Add style to home body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component.js';
+import { HomeBodyComponent } from './home-body/home-body.component.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeBodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
