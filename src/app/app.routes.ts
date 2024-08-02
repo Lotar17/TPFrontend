11e1ef34534ab8450ad985ebbdc06c9e9ce36c88
Valentin Bertone <bertonev17@gmail.com>
2024-08-02 19:54:51 -0300

    Add login component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { LoginComponent } from './login/login.component.js';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login', component :LoginComponent}
+];
