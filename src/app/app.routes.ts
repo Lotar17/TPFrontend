43672dbbb4599f50d40d929f64377f0b6cd0cec0
Ignacio Di Martino <nachojdimartino@gmail.com>
2024-09-26 20:03:41 -0300

    Agrego filtros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component.js';
 import { HomeBodyComponent } from './home-body/home-body.component.js';
 import { HomeComponent } from './home/home.component.js';
 import { DashboardComponent } from './dashboard/dashboard.component.js';
+import { ClienteComponent } from './cliente/cliente.component.js';
 
 export const routes: Routes = [
     {
@@ -13,5 +14,9 @@ export const routes: Routes = [
     },
     {
         path:"", component:HomeComponent,
+    },
+    {
+        path:"cliente",
+        component:ClienteComponent
     }
 ];
