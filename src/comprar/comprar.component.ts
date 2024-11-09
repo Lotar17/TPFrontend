b43dd401585dad5f85dee573769ae877c471d489
Ignacio Di Martino <nachojdimartino@gmail.com>
2024-11-09 09:09:30 -0300

    Agrego calculo del precio actual a la compra

--- src/comprar/comprar.component.ts
+++ src/comprar/comprar.component.ts
@@ -7,11 +7,13 @@ import { PersonaService } from '../app/api/per.service.js';
 import { CRUDService } from '../app/api/crud.service.js';
 import { ReactiveFormsModule } from '@angular/forms';
 import { firstValueFrom } from 'rxjs'; // Para convertir el Observable en Promise
+import { CommonModule } from '@angular/common';
+import { HistoricoPrecioService } from '../app/api/calculaprecio.service.js';
 
 @Component({
   selector: 'app-comprar',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './comprar.component.html',
   styleUrls: ['./comprar.component.css'],
 })
@@ -24,25 +26,65 @@ export class ComprarComponent {
     nombre_persona: new FormControl(), // Campo para ingresar el nombre de la persona
     nombre_empleado: new FormControl(), // Campo para ingresar el nombre del empleado
   });
-
+  errorMessage: string = '';
   productoId: string; // ID del producto desde la URL
+  precioActual: number | undefined;
+
 
   constructor(
     private route: ActivatedRoute, // Para capturar el ID del producto
     private crudService: CRUDService<Compra>,
     private empleadoService: EmpleadoService,
-    private personaService: PersonaService
+    private personaService: PersonaService,
+    private historicoprecioService: HistoricoPrecioService,
   ) {
     // Obtener el ID del producto desde la ruta activa
     this.productoId = this.route.snapshot.paramMap.get('id') || '';// el problema esta acanpm
   }
 
+
+  validarCantidadProducto(form: FormGroup): boolean {
+    if (form.value.cantidad_producto! <= 0) {
+      this.errorMessage = 'La cantidad de producto debe ser mayor a 0.';
+      return false;
+    }
+  
+    // Limpia el mensaje de error si la validación pasa
+    this.errorMessage = '';
+    return true;
+  }
+  
+
+
   async onSubmit() {
+    if (!this.validarCantidadProducto(this.publicaForm)) {
+      // Detener si la cantidad no es válida
+      return;
+    }
+
+
+    
+    
     try {
-      // Obtener el ID del empleado y persona por nombre utilizando firstValueFrom para convertir los Observables en Promises
+
       const empleadoId = await firstValueFrom(this.empleadoService.getEmpleadoIdById(this.publicaForm.value.nombre_empleado!));
       const personaId = await firstValueFrom(this.personaService.getPersonaIdById(this.publicaForm.value.nombre_persona!));
+    
 
+      this.historicoprecioService.getOne(this.productoId).subscribe(
+        (valor) => {
+          if (valor !== undefined) {
+            this.precioActual = valor*this.publicaForm.value.cantidad_producto;
+          } else {
+            console.log('No se encontró el precio histórico');
+          }
+        },
+        (error) => {
+          console.error('Error al obtener el precio:', error);
+        }
+      );
+      
+      
       const compra: Compra = {
         direccion_entrega: this.publicaForm.value.direccion_entrega!,
         cantidad_producto: this.publicaForm.value.cantidad_producto!,
