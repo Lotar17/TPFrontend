445f7bbb05740a6364740d1234d90573d0242209
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2025-01-14 00:46:22 -0300

    feat:add autenticacion.service.ts

--- src/app/api/autenticacion.service.ts
+++ src/app/api/autenticacion.service.ts
@@ -2,13 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiResponse } from '../models/ApiResponse.js';
 import { firstValueFrom } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { SesionPersona } from '../models/sesionPersona.entity.js';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AutenticacionService {
+  private subject = new BehaviorSubject<SesionPersona | null>(null);
+  $ = this.subject.asObservable();
   url = 'http://localhost:3000/login/checkPermissions';
+
   constructor(private http: HttpClient) {}
+
+  getUserInformation() {
+    let datosUsuario: SesionPersona | undefined = undefined;
+    this.http
+      .get<ApiResponse<SesionPersona>>(
+        'http://localhost:3000/login/getUserInformation',
+        { withCredentials: true }
+      )
+      .subscribe((response) => {
+        if (response.data !== undefined) this.subject.next(response.data);
+      });
+  }
 
   async getRolByCookie(): Promise<string | undefined> {
     try {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,28 @@
-import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component.js';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
-import { RegisterComponent } from '../register/register.component.js';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AutenticacionService } from '../api/autenticacion.service.js';
+import { SesionPersona } from '../models/sesionPersona.entity.js';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LoginComponent,RegisterComponent,RouterLink,RouterOutlet],
+  imports: [RouterLink, AsyncPipe, NgIf],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-  constructor(private route:Router){
+export class HeaderComponent implements OnInit {
+  sesionPersona$ = this.autenticacionService.$;
+  constructor(
+    private route: Router,
+    private autenticacionService: AutenticacionService
+  ) {}
+
+  ngOnInit() {
+    this.autenticacionService.getUserInformation();
+    console.log(`Persona ${JSON.stringify(this.sesionPersona$)}`);
   }
-  irLogin(path:string):void{
-    this.route.navigateByUrl(path)
+  irLogin(path: string): void {
+    this.route.navigateByUrl(path);
   }
 }
