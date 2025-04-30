290e4c1e9b62e616db289dc5a030ec00f199c73f
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2025-04-30 10:20:10 -0300

    fix: bugs de merge

--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { LoginService } from '../api/login.service.js';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
-import { RouterLink } from '@angular/router';
-import { Router } from '@angular/router';
-
 import { response } from 'express';
 import { Router, RouterLink } from '@angular/router';
 
@@ -16,9 +13,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-inicio-sesion',
   standalone: true,
 
-  imports: [ReactiveFormsModule,RouterLink,CommonModule],
-
-  imports: [ReactiveFormsModule,RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule],
 
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css',
@@ -26,49 +21,53 @@ import { CommonModule } from '@angular/common';
 export class InicioSesionComponent {
   loginSuccesful: boolean | undefined;
 
-inicioExitoso:boolean=false
+  inicioExitoso: boolean = false;
 
   loginForm = new FormGroup({
     mail: new FormControl(),
     password: new FormControl(),
   });
 
-  constructor(private loginService: LoginService,private authService:AuthService, private router: Router,
-    private autenticacionService:AutenticacionService
+  constructor(
+    private loginService: LoginService,
+    private authService: AuthService,
+    private router: Router,
+    private autenticacionService: AutenticacionService
   ) {}
 
   async onSubmit() {
     const credentials = {
       mail: this.loginForm.value.mail ?? '',
       password: this.loginForm.value.password ?? '',
-    };try {
+    };
+    try {
       (await this.loginService.login(credentials)).subscribe({
         next: (response) => {
-          this.inicioExitoso=true
+          this.inicioExitoso = true;
           if (response.result) {
             console.log(response.message);
-            
+
             // Seteamos el ID del usuario en el servicio
             this.authService.setUserId(response.usuarioId);
-    
+
             // Ahora llamamos a getUserInformation para asegurarnos de que los datos del usuario estén actualizados
             this.autenticacionService.getUserInformation().subscribe({
               next: (userResponse) => {
-              
-               
-                if (response.userRol === "Administrador") {
+                if (response.userRol === 'Administrador') {
                   this.router.navigateByUrl('/admin');
-                } else if (response.userRol.toLowerCase() === "usuario") {
+                } else if (response.userRol.toLowerCase() === 'usuario') {
                   this.router.navigateByUrl('/productos');
                 } else {
                   this.router.navigateByUrl('panelSeguimientoEmpleado');
                 }
               },
               error: (error) => {
-                console.error('Error al obtener la información del usuario', error);
-              }
+                console.error(
+                  'Error al obtener la información del usuario',
+                  error
+                );
+              },
             });
-    
           } else {
             this.loginSuccesful = false;
             // Aquí puedes manejar el caso en que el login no sea exitoso
@@ -77,12 +76,11 @@ inicioExitoso:boolean=false
         error: (error) => {
           console.error('Error en la autenticación', error);
           this.loginSuccesful = false;
-        }
+        },
       });
     } catch (error) {
       console.error('Error al intentar loguearse', error);
       this.loginSuccesful = false;
     }
-    
-
-}}
+  }
+}
